084c5f1081d9535d726847eab168523fa0523f90
MelissaKroese <mel@ixperience.co.za>
2017-09-08 15:34:38 +0200

    updates

--- myapp.rb
+++ myapp.rb
@@ -7,7 +7,7 @@ end
 
 post '/thank_you' do
   CSV.open("leads.csv", "a") do |csv|
-    csv << [params[:firstname], params[:lastname], params[:email]]
+    csv << [params[:firstname], params[:lastname], params[:email], params[:created_at]]
   end
 
   erb :thank_you, locals: {
@@ -18,8 +18,13 @@ end
 
 get '/admin' do
   leads = CSV.read('leads.csv')
+
+  leads.sort! { |x, y| x[0] <=> y[0] }
+
   erb :admin, locals: { leads: leads }
+
 end
+
 
 get '/leads.csv' do
   content_type 'application/csv'
